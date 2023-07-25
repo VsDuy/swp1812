9d0e37aaa8f76b6b7a39c7ea5e76f3e85c0ef0f8
Tung <132988730+TungTran17@users.noreply.github.com>
2023-07-26 06:39:03 +0700

    z

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -94,7 +94,6 @@ export default function SignUp() {
                         <option value="">Choose option</option>
                         <option value={0}>Female</option>
                         <option value={1}>Male</option>
-                        <option value={2}>Other</option>
                       </Form.Control>
                     </Form.Group>
                   </Col>
